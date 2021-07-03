8dd6e90fb971bd9b17bbdff59ee7e6225b82bf62
James Sturtevant <jstur@microsoft.com>
2021-07-02 23:28:36 -0700

    almost

--- kubeadm/flannel/start.ps1
+++ kubeadm/flannel/start.ps1
@@ -1,22 +1,29 @@
 $ErrorActionPreference = "Stop";
 
 # # make sure external network exists to create switch for flannel otherwise it drops connection on 
-# # https://github.com/coreos/flannel/issues/1359
-# # https://github.com/kubernetes-sigs/sig-windows-tools/issues/103#issuecomment-709426828
-# ipmo hns.psm1
-# $network = Get-HNSNetwork | ? Name -eq "External"
-# if ($network -eq $null) {
-#   New-HNSNetwork -Type Overlay -AddressPrefix "192.168.255.0/30" -Gateway `
-#     "192.168.255.1" -Name "External" -AdapterName "Ethernet 2" -SubnetPolicies @(@{Type = "VSID"; VSID = 9999; }); `+
-# } elseif ($network.Type -ne "Overlay") {
-#   Write-Warning "'External' network already exists but has wrong type: $($network.Type)." 
-# }
+# https://github.com/coreos/flannel/issues/1359
+# https://github.com/kubernetes-sigs/sig-windows-tools/issues/103#issuecomment-709426828
+ipmo $env:CONTAINER_SANDBOX_MOUNT_POINT/flannel/hns.psm1
+$network = Get-HNSNetwork | ? Name -eq "External"
+if ($network -eq $null) {
+  New-HNSNetwork -Type Overlay -AddressPrefix "192.168.255.0/30" -Gateway "192.168.255.1" -Name "External" -AdapterName "Ethernet 3" -SubnetPolicies @(@{Type = "VSID"; VSID = 9999; });
+} elseif ($network.Type -ne "Overlay") {
+  Write-Warning "'External' network already exists but has wrong type: $($network.Type)." 
+}
 
 # flannel uses host-local for ipam so copy that to the correct location
+Write-Host "copy cni bins"
 cp -force -recurse $env:CONTAINER_SANDBOX_MOUNT_POINT/cni/* c:/opt/cni/bin
+
+Write-Host "copy flannel config"
+mkdir -force C:\etc\kube-flannel\
+ls C:\etc\kube-flannel\
+ls $env:CONTAINER_SANDBOX_MOUNT_POINT/etc/kube-flannel/
+cp -force $env:CONTAINER_SANDBOX_MOUNT_POINT/etc/kube-flannel/net-conf.json  C:\etc\kube-flannel\net-conf.json
 
 # configure cni
 # get info
+Write-Host "update cni config"
 $cniJson = get-content $env:CONTAINER_SANDBOX_MOUNT_POINT/etc/kube-flannel-windows/cni-conf-containerd.json | ConvertFrom-Json
 $serviceSubnet = get-content $env:CONTAINER_SANDBOX_MOUNT_POINT/etc/kubeadm-config/ClusterConfiguration | ForEach-Object -Process {if($_.Contains("serviceSubnet:")) {$_.Trim().Split()[1]}}
 $podSubnet = get-content $env:CONTAINER_SANDBOX_MOUNT_POINT/etc/kubeadm-config/ClusterConfiguration | ForEach-Object -Process {if($_.Contains("podSubnet:")) {$_.Trim().Split()[1]}}
@@ -31,6 +38,12 @@ Set-Content -Path c:/etc/cni/net.d/10-flannel.conf ($cniJson | ConvertTo-Json -d
 
 # set route for metadata servers in clouds
 # https://github.com/kubernetes-sigs/sig-windows-tools/issues/36
+Write-Host "add route"
 route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0
 
-& $env:CONTAINER_SANDBOX_MOUNT_POINT/flanneld.exe --kube-subnet-mgr --kubeconfig-file c:/k/kubeconfig.yml
+write-host "copy sa info (should be able to do this with a change to go client"
+mkdir -force $env:CONTAINER_SANDBOX_MOUNT_POINT/flannel-config-file/var/run/secrets/kubernetes.io/serviceaccount/
+cp -force $env:CONTAINER_SANDBOX_MOUNT_POINT/var/run/secrets/kubernetes.io/serviceaccount/* $env:CONTAINER_SANDBOX_MOUNT_POINT/flannel-config-file/var/run/secrets/kubernetes.io/serviceaccount/
+
+Write-Host "Starting flannel"
+& $env:CONTAINER_SANDBOX_MOUNT_POINT/flannel/flanneld.exe --kube-subnet-mgr --kubeconfig-file $env:CONTAINER_SANDBOX_MOUNT_POINT/flannel-config-file/kubeconfig.conf --iface "Ethernet 3"
